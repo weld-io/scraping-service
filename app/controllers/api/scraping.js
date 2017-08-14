b4413bc05b9a6d7fcdf45423606bb14d2c0ecfa7
Tom Söderlund <tom@tomsoderlund.com>
2017-08-14 14:13:41 +0200

    Working scraper - hard-coded

--- app/controllers/api/scraping.js
+++ app/controllers/api/scraping.js
@@ -8,13 +8,36 @@
 
 const express = require('express');
 const _ = require('lodash');
+const scraperjs = require('scraperjs');
 
 const helpers = require('../../config/helpers');
 
 const scraping = {
 
 	read: function (req, res, next) {
-		res.json({});
+		const url = decodeURIComponent(req.query.url);
+		const selector = decodeURIComponent(req.query.selector);
+		const parseFunction = function ($) {
+			const selector = '.title a';
+			console.log('parseFunction', selector);
+			return $(selector).map(function() {
+				return $(this).text();
+			}).get();
+		}
+		const parseFunction2 = parseFunction.bind(null, selector);
+		console.log(`Scrape: "${url}", "${selector}"`);
+		try {
+			scraperjs.DynamicScraper.create(url)
+				.scrape(parseFunction)
+				.then(function (results) {
+					//console.log(results);
+					res.json({ results: results });
+				});
+		}
+		catch (err) {
+			res.status(400).json({ error: err });
+		}
+
 	},
 
 }
