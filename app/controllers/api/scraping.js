b50bba168dc5d7051782932e3b979e40ef0fccca
Tom Söderlund <tom@tomsoderlund.com>
2018-01-31 14:36:48 +0100

    Refactor scrapeChrome

--- app/controllers/api/scraping.js
+++ app/controllers/api/scraping.js
@@ -61,58 +61,57 @@ const parseDOM = (domString, pageSel, complete, deep) => {
 	return resultArray;
 };
 
-const scraping = {
+const scrapeChrome = function (req, res, next) {
+	const pageUrl = decodeURIComponent(req.query.url);
+	const pageSelector = decodeURIComponent(req.query.selector || 'body');
+	const loadExtraTime = req.query.time || 0;
+	const deepResults = req.query.deep || false;
+	const completeResults = req.query.complete || false;
+	const timeStart = Date.now();
 
-	scrapeChrome: function (req, res, next) {
-		const pageUrl = decodeURIComponent(req.query.url);
-		const pageSelector = decodeURIComponent(req.query.selector || 'body');
-		const loadExtraTime = req.query.time || 0;
-		const deepResults = req.query.deep || false;
-		const completeResults = req.query.complete || false;
-		const timeStart = Date.now();
+	console.log(`Scrape: "${pageUrl}", "${pageSelector}", ${loadExtraTime} ms`);
 
-		console.log(`Scrape: "${pageUrl}", "${pageSelector}", ${loadExtraTime} ms`);
+	const CDP = require('chrome-remote-interface');
+	CDP((client) => {
+		// Extract used DevTools domains.
+		const {Page, Runtime} = client;
 
-		const CDP = require('chrome-remote-interface');
-		CDP((client) => {
-			// Extract used DevTools domains.
-			const {Page, Runtime} = client;
-
-			// Enable events on domains we are interested in.
-			Promise.all([
-				Page.enable()
-			]).then(() => {
-				return Page.navigate({ url: pageUrl });
-			});
-
-			// Evaluate outerHTML after page has loaded.
-			Page.loadEventFired(() => {
-				setTimeout(() => {
-					Runtime.evaluate({ expression: 'document.body.outerHTML' }).then((result) => {
-						const selectorsArray = pageSelector.split(',');
-						const resultsObj = selectorsArray.map((sel) => {
-							const resultArray = parseDOM(result.result.value, sel, completeResults, deepResults);
-							return { selector: sel, count: resultArray.length, items: resultArray };
-						});
-						const timeFinish = Date.now();
-						client.close();
-						res.json({ time: (timeFinish-timeStart), results: resultsObj });
-					});
-				}, loadExtraTime); // extra time before accessing DOM
-			});
-		}).on('error', (err) => {
-			console.error('Cannot connect to browser:', err);
-			res.status(400).json({ error: err });
+		// Enable events on domains we are interested in.
+		Promise.all([
+			Page.enable()
+		]).then(() => {
+			return Page.navigate({ url: pageUrl });
 		});
-	},
 
-}
+		// Evaluate outerHTML after page has loaded.
+		Page.loadEventFired(() => {
+			setTimeout(() => {
+				Runtime.evaluate({ expression: 'document.body.outerHTML' }).then((result) => {
+					const selectorsArray = pageSelector.split(',');
+					const resultsObj = selectorsArray.map((sel) => {
+						const resultArray = parseDOM(result.result.value, sel, completeResults, deepResults);
+						return { selector: sel, count: resultArray.length, items: resultArray };
+					});
+					const timeFinish = Date.now();
+					client.close();
+					res.json({ time: (timeFinish-timeStart), results: resultsObj });
+				});
+			}, loadExtraTime); // extra time before accessing DOM
+		});
+	}).on('error', (err) => {
+		console.error('Cannot connect to browser:', err);
+		res.status(400).json({ error: err });
+	});
+};
+
+
+// Routes
 
 module.exports = function (app, config) {
 
 	const router = express.Router();
 	app.use('/', router);
 
-	router.get('/api/scrape', scraping.scrapeChrome);
+	router.get('/api/scrape', scrapeChrome);
 
 };
