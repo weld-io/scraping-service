f15251ba02a9f532eb0af0aef8ec1432df3cfa7b
Tom Söderlund <tom@tomsoderlund.com>
2018-11-10 16:02:53 +0100

    Fix tests

--- test/app.js
+++ test/app.js
@@ -4,12 +4,25 @@ const test = require('tape');
 const request = require('supertest');
 const async = require('async');
 
-test('Test the entire API', function (assert) {
+test('Test /api/scrape', function (assert) {
 	const app = require('../app/app');
 	async.waterfall([
 			(cb) => request(app).get('/api/scrape?url=https://news.ycombinator.com&selector=.title+a').expect(200, cb),
 			(results, cb) => { assert.ok(results.body, 'Returned list'); cb(null, results); },
-			(results, cb) => { assert.ok(results.body.count > 0, 'More than zero entries'); cb(null, results); },
+			(results, cb) => { assert.ok(results.body.results[0].count > 0, '1+ items returned'); cb(null, results); },
+		],
+		(err, results) => {
+			assert.end();
+		}
+	);
+});
+
+test('Test /api/metadata', function (assert) {
+	const app = require('../app/app');
+	async.waterfall([
+			(cb) => request(app).get('/api/meta?url=https://www.weld.io').expect(200, cb),
+			(results, cb) => { assert.ok(results.body, 'Returned results'); cb(null, results); },
+			(results, cb) => { assert.ok(results.body.general.title.indexOf('Weld') !== -1, 'Page <title> contains “Weld”'); cb(null, results); },
 		],
 		(err, results) => {
 			assert.end();
@@ -27,23 +40,23 @@ test.only(name, cb)
 test.createStream().pipe(process.stdout);
 test.createStream({ objectMode: true }).on('data', function (row) { console.log(JSON.stringify(row)) });
 
-t.plan(n)
-t.end(err)
-t.fail(msg)
-t.pass(msg)
-t.timeoutAfter(ms)
-t.skip(msg)
-t.ok(value, msg)
-t.notOk(value, msg)
-t.error(err, msg)
-t.equal(actual, expected, msg)
-t.notEqual(actual, expected, msg)
-t.deepEqual(actual, expected, msg)
-t.notDeepEqual(actual, expected, msg)
-t.deepLooseEqual(actual, expected, msg)
-t.notDeepLooseEqual(actual, expected, msg)
-t.throws(fn, expected, msg)
-t.doesNotThrow(fn, expected, msg)
-t.test(name, [opts], cb)
-t.comment(message)
+assert.plan(n)
+assert.end(err)
+assert.fail(msg)
+assert.pass(msg)
+assert.timeoutAfter(ms)
+assert.skip(msg)
+assert.ok(value, msg)
+assert.notOk(value, msg)
+assert.error(err, msg)
+assert.equal(actual, expected, msg)
+assert.notEqual(actual, expected, msg)
+assert.deepEqual(actual, expected, msg)
+assert.notDeepEqual(actual, expected, msg)
+assert.deepLooseEqual(actual, expected, msg)
+assert.notDeepLooseEqual(actual, expected, msg)
+assert.throws(fn, expected, msg)
+assert.doesNotThrow(fn, expected, msg)
+assert.test(name, [opts], cb)
+assert.comment(message)
 */
