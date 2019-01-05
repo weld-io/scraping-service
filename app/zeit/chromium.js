cc737fd40fdfb68b3a53cb13c2641a1a1726581f
Tom Söderlund <tom@tomsoderlund.com>
2019-01-05 11:29:27 +0100

    Add Zeit example

---
+++ app/zeit/chromium.js
@@ -0,0 +1,18 @@
+const chrome = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
+
+async function getScreenshot (url, type, quality, fullPage) {
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless
+  })
+
+  const page = await browser.newPage()
+  await page.goto(url)
+  const file = await page.screenshot({ type, quality, fullPage })
+  await browser.close()
+  return file
+}
+
+module.exports = { getScreenshot }
